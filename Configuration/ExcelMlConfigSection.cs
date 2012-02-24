817dc042ad7ef883688fac1313f1ba2333ac5fe8
architect0317 <peter.adams@pingdevelopment.com>
2012-02-24 11:10:56 -0700

    Update Configuration/ExcelMlConfigSection.cs

--- Configuration/ExcelMlConfigSection.cs
+++ Configuration/ExcelMlConfigSection.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Configuration;
 
-namespace WellsFargo.ExcelML.Configuration
+namespace PingDevelopment.ExcelML.Configuration
 {
     public class ExcelMlConfigSection : ConfigurationSection
     {
