e50d5ee1262e6b84c2e011afc7787f1d3540612e
architect0317 <peter.adams@pingdevelopment.com>
2012-02-24 11:11:27 -0700

    Update Configuration/StringDataElement.cs

--- Configuration/StringDataElement.cs
+++ Configuration/StringDataElement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace WellsFargo.ExcelML.Configuration
+namespace PingDevelopment.ExcelML.Configuration
 {
     public class StringDataElement : BaseFormatElement
     {
